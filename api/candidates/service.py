13107246a95f54b639b4c35bc961f6e94215eb1c
akashjobanputra <akash.jobanputra@hotmail.com>
2024-07-25 17:07:25 +0530

    Improved batch fetching, removed recursion

--- api/candidates/service.py
+++ api/candidates/service.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 from candidates.schemas import CandidatesList
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from pymongo import UpdateOne
@@ -34,39 +34,69 @@ class CandidateSerice:
                 
     async def sync_candidates(self) -> Tuple[Optional[str], int]:
         url = f"{self.api_url}/candidates"
-        
-        async def fetch_and_store_candidates(url: str):
-            response = self.session.get(url)
-            response.raise_for_status()
-            data = response.json()
-            candidates = data.get('candidates', [])
-            db = await self.get_database()
-            
-            if candidates:
-                # Prepare bulk operations for upsert
-                operations = [
-                    UpdateOne(
-                        {"id": candidate["id"]},
-                        {"$setOnInsert": candidate},
-                        upsert=True
-                    ) for candidate in candidates
-                ]
-                
-                # Perform bulk write
-                result = await db.candidates.bulk_write(operations)
-                
-                print(f"Matched: {result.matched_count}, "
-                      f"Modified: {result.modified_count}, "
-                      f"Upserted: {result.upserted_count}")
 
-            # Check for paging and fetch next page if exists
-            next_page = data.get('paging', {}).get('next')
-            if next_page:
-                await asyncio.sleep(1)  # Delay for 1 second before the next request
-                await fetch_and_store_candidates(next_page)
+        async def fetch_candidates(url: str) -> Tuple[List[dict], Optional[str]]:
+            try:
+                response = self.session.get(url)
+                response.raise_for_status()
+                data = response.json()
+                candidates = data.get('candidates', [])
+                return candidates, data.get('paging', {}).get('next')
+            except Exception as e:
+                error_msg = f"Error fetching candidates: {str(e)}"
+                print(error_msg)
+                raise e
+
+        async def store_candidates(candidates: List[dict]) -> Tuple[int, int, int]:
+            try:
+                db = await self.get_database()
+                
+                if candidates:
+                    # Prepare bulk operations for upsert
+                    operations = [
+                        UpdateOne(
+                            {"id": candidate["id"]},
+                            {"$setOnInsert": candidate},
+                            upsert=True
+                        ) for candidate in candidates
+                    ]
+                    
+                    # Perform bulk write
+                    result = await db.candidates.bulk_write(operations)
+                    
+                    print(f"Matched: {result.matched_count}, "
+                          f"Modified: {result.modified_count}, "
+                          f"Upserted: {result.upserted_count}")
+                    
+                    return result.matched_count, result.modified_count, result.upserted_count
+                else:
+                    return 0, 0, 0
+            except Exception as e:
+                error_msg = f"Error storing candidates: {str(e)}"
+                print(error_msg)
+                raise e
+
+        async def sync_candidates(url) -> Tuple[Optional[str], int]:
+            input_url = url            
+            try:
+                while input_url:
+                    candidates, next_page = await fetch_candidates(input_url)
+                    await store_candidates(candidates)
+                    
+                    if next_page:
+                        await asyncio.sleep(1)  # Delay for 1 second before the next request
+                        input_url = next_page
+                    else:
+                        input_url = None
+                
+                return None, 200
+            except Exception as e:
+                error_msg = f"Error syncing candidates: {str(e)}"
+                print(error_msg)
+                raise e
 
         try:
-            await fetch_and_store_candidates(url)
+            await sync_candidates(url)
             return None, 200
         except Exception as e:
             error_msg = f"Error syncing candidates: {str(e)}"
